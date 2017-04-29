02a5563980215f68f3730426bb18e861591cd054
move file <Kim@Kims-MacBook-Pro.local>
2017-04-29 11:58:06 +0200

    Write my first hello world in java

--- MeinErstesJavaProject/src/main/Hello.java
+++ MeinErstesJavaProject/src/main/Hello.java
@@ -2,9 +2,16 @@ package main;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		plus(2,6);
+	}
+	
+	public static int plus(int a,int b)
+	{
+		System.out.println(a+b);
+		return a+b;
 	}
 
 }
