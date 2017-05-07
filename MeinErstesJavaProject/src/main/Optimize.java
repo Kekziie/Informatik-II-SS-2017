7fa0de48467e2044fc5e10e81e3af3203ae1bc3c
move file <Kim@Kims-MBP.fritz.box>
2017-05-07 17:29:14 +0200

    optimized

--- MeinErstesJavaProject/src/main/Optimize.java
+++ MeinErstesJavaProject/src/main/Optimize.java
@@ -1,39 +1,56 @@
 package main;
 
+
 public class Optimize {
 
 	public static final double A = 5;
 	public static final double B = -2;
-	
+
 	public static void main(String[] args) {
 	}
 
 	public static double optimize(int func, double x0) {
-		
-		; // TODO - implement your optimization procedure here.
-		
-		return 0.00;// TODO - return the actual result here
+		double result = 0;
+
+		double x1=0;
+		while (Math.abs(x0-(x0 - 0.001 * numericalGradient(func,x0)))>Math.pow(10, -9))
+		{
+
+			x1=x0;
+			x0=(x1-0.001*numericalGradient(func,x1));
+		}
+		result = x1;
+
+		return result;// TODO - return the actual result here
 	}
-	
+
 	public static double evalFunc(int func, double x) {
 		if(func==0)
 			return f1(x);
-		return f2(x);				
+		return f2(x);                                
 	}
-	
+
 	public static double f1(double x) {
-		
-		return 0; // TODO - return the actual function value of f1 here.		
+		return 1.84+1.42*x-2.4*Math.pow(x, 2)+0.91*Math.pow(x, 3)-0.124*Math.pow(x, 4)+ 0.0055*Math.pow(x, 5);
+
 	}
-	
+
 	public static double f2(double x) {
-		return 0; // TODO - return the actual function value of f2 here.
-	}	
+		x=x-A;
+		double result = Math.pow(x, 2)-Math.abs(B);
+		return result;
+	}        
 
 	public static double numericalGradient(int func, double x) {
 		; // TODO calculate the numerical gradient
-		
-		return 0 ; // TODO return the determined gradient here.
-	}
 
+		double result = 0;
+		double h = 0.00001;
+		if(func==0) result= (f1(x+h)-f1(x-h))/(2*h);
+		else result= (f2(x+h)- f2(x-h))/(2*h);
+
+
+		return result;
+
+	}
 }
