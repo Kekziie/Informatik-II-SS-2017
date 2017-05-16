79895115748eaa351538ce66fd955f9a11ef6c93
move file <Kim@Kims-MacBook-Pro.local>
2017-05-16 19:31:31 +0200

    Add new class and task

---
+++ MeinErstesJavaProject/src/main/NaiveSorting.java
@@ -0,0 +1,83 @@
+package main;
+
+import java.util.Arrays;
+
+public class NaiveSorting {
+
+	public static void main(String[] args) {
+		// TODO: you can test your solution like this:
+		
+//		int[] myArray = new int[10];
+//		for(int i=0; i<myArray.length; i++)
+//			myArray[i] = (int)(Math.random()*100);
+//		
+//		System.out.println("Testing array:" + Arrays.toString(myArray));
+//				
+//		System.out.println("Minimum: "+getMinimum(myArray));
+//		System.out.println("Maximum: "+getMaximum(myArray));
+//		System.out.println("Mean: "+getMean(myArray));
+//		
+//		System.out.println(getArrayString(myArray,3));
+//		
+//		sortArray(myArray);
+//		
+//		System.out.println(getArrayString(myArray, 5));
+	}
+
+	/**
+	 * Gets the minimum value in the array and return its value.
+	 * 
+	 * @param array
+	 * @return the minimum
+	 */
+	public static int getMinimum(int[] array) {	
+		// TODO a)
+		return 0; // return the determined value!		
+	}
+
+	/**
+	 * Get the maximum value in the array and return its value.
+	 * 
+	 * @param array
+	 * @return the maximum
+	 */
+	public static int getMaximum(int[] array) {	
+		// TODO a)
+		return 0; // return the determined value!		
+	}
+
+	/**
+	 * Gets the mean value in the array and return its value.
+	 * 
+	 * @param array
+	 * @return the maximum
+	 */
+	public static double getMean(int[] array) {	
+		// TODO b)
+		return 0; // return the determined value!		
+	}
+
+	/**
+	 * Sorts the array in increasing order.
+	 */
+	public static void sortArray(int[] array) {
+
+		// TODO c)
+	}
+
+	/**
+	 * Converts the array into a formatted string.
+	 * 
+	 * @param array the array
+	 * @param numPerLine the array entries in the string per line
+	 * @return the string representation.
+	 */
+	public static String getArrayString(int[] array, int numPerLine) {
+
+		// TODO d
+		return ""; // return the generated String here.
+	}
+
+
+}
+
--- MeinErstesJavaProject/src/main/Paperworks.java
+++ MeinErstesJavaProject/src/main/Paperworks.java
@@ -1,12 +1,16 @@
 package main;
 
 public class Paperworks {
-	int i = 5;
-	short s = 3;
-	float f = 5;
-	char a = 'a';
-	double d = 18.8;
-	long l = 34;
-	byte by = 1;
+	
 
-}
+	public static void main(String[] args) {
+		int value = 6;
+	    int sum = 0;
+	    for(int index = 0; index < 8; index++) {
+	    sum += value;
+	    value += 3;
+	    index += 2;
+	    }
+	        System.out.println(sum);
+	}
+	}
